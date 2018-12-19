5d3e43840d9fd3959b2e71f412f47251e447595f
Victor-Onanuga <44228722+Victor-Onanuga@users.noreply.github.com>
2018-12-19 18:39:06 +0000

    added football image to mainpage

--- MobileApp/MobileApp/MainPage.xaml.cs
+++ MobileApp/MobileApp/MainPage.xaml.cs
@@ -12,21 +12,34 @@ namespace MobileApp
         public MainPage()
         {
             InitializeComponent();
+            SetUpImages();
         }
 
-        private void PremierLeague_Clicked(object sender, EventArgs e)//Premier league button
+        private void SetUpImages()
+        {
+            // function to set up images
+            var assembly = typeof(MainPage);
+
+
+
+            string strFilename = "MobileApp.Assets.football.png";
+            football.Source = ImageSource.FromResource(strFilename, assembly);
+        }
+
+
+            private void PremierLeague_Clicked(object sender, EventArgs e)//Premier league button
         {
             Navigation.PushAsync(new PremierLeague());
         }
 
         private void ChampionsLeague(object sender, EventArgs e)//Champions League button
         {
-            
+            Navigation.PushAsync(new ChampionsLeague());
         }
 
         private void WorldCup(object sender, EventArgs e)//World Cup button
         {
-
+            Navigation.PushAsync(new WorldCup());
         }
 
         private void AirtricityLeague(object sender, EventArgs e)//Airtricity Button
@@ -36,8 +49,33 @@ namespace MobileApp
 
         private void BackButton(object sender, EventArgs e)//Back Button
         {
-
+            Navigation.PushAsync(new MainPage());
         }
-       
+
+        /*nitializeComponent ();
+            SetUpImages();
+		}
+
+        private void SetUpImages()
+        {
+            // function to set up images
+            var assembly = typeof(MainPage);
+            
+
+            string strFilename = "FinalAppProject.Assets.Images.Tyr.png";
+            imageTyr.Source = ImageSource.FromResource(strFilename, assembly);
+
+
+            string strFilename2 = "FinalAppProject.Assets.Images.TyrSymbol.png";
+            imageTyrSymbol.Source = ImageSource.FromResource(strFilename2, assembly);
+
+
+            string strFilename3 = "FinalAppProject.Assets.Images.Tyr2.jpg";
+            imageTyr2.Source = ImageSource.FromResource(strFilename3, assembly);
+
+
+            string strFilename4 = "FinalAppProject.Assets.Images.Tyr3.jpg";
+            imageTyr3.Source = ImageSource.FromResource(strFilename4, assembly);*/
+
     }
 }
